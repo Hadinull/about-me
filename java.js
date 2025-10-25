e3e9bfa21bf07b18cd5a48d9a867ba2033dcdc32
Hadinull <Hadinull.3000@gmail.com>
2025-10-25 11:43:30 +0300

    first 1

---
+++ java.js
@@ -0,0 +1,12 @@
+const button = document.getElementById('button');
+const travel = document.getElementById('travel');
+
+button.onclick = function() {
+    button.innerHTML = '<a>https://github.com/Hadinull/git-random-message-</a>'
+    button.style.color = 'blue'
+}
+
+travel.onclick = function() {
+    travel.innerHTML = '<a>https://github.com/Hadinull/project-travel-company</a>'
+    travel.style.color = 'blue'
+}
